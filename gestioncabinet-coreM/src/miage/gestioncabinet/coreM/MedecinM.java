e019b5c6ae420a3a3a2e7b1ddeda58b9402e1e88
a900581 <a900581@CAN-S109-PC16.salsa.univ-amu.fr>
2014-10-25 12:01:13 +0200

    implémentation des interface Medecin et Produit

---
+++ gestioncabinet-coreM/src/miage/gestioncabinet/coreM/MedecinM.java
@@ -0,0 +1,51 @@
+package miage.gestioncabinet.coreM;
+
+import miage.gestioncabinet.api.Medecin;
+
+public class MedecinM implements Medecin {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2795187985951315891L;
+	private String RPPS;
+	private String nom;
+	private String prenom;
+	private String compte;
+	
+	
+
+	@Override
+	public String getCompte() {
+		return this.compte;
+	}
+
+	@Override
+	public String getNom() {
+		return this.nom;
+	}
+
+	@Override
+	public void setNom(String nom) {
+		this.nom = nom;
+		
+	}
+
+	@Override
+	public String getPrenom() {
+		return this.prenom;
+	}
+
+	@Override
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+		
+	}
+
+	@Override
+	public String getRPPS() {
+		return this.RPPS;
+	}
+
+	
+}
